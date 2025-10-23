5dede59c1e75150720ebacffe714edfd3eb1b054
K4rin <karinecris.mix@gmail.com>
2025-10-23 14:25:03 -0300

    Menu e rodapé

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Home from "./pages/home/Home";
-
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,79 +3,35 @@ function Home() {
     return (
         <>
         
-        <div
-            style={{
-                backgroundColor: "#312e81",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+        <div className ="bg-indigo-900 flex justify-center">
                 
-                <div
-                    style={{
-                        display: "grid",
-                        gridTemplateColumns: "1fr 1fr",
-                        color: "white",
-                        width: "100%",
-                        maxWidth: "1280px",
-                    }}>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "1rem",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            paddingTop: "1rem",
-                            paddingBottom: "1rem",
-                        }}>
-                        <h2
-                            style={{
-                                fontSize: "3rem",
-                                fontWeight: "bold",
-                            }}
-                            >
+                <div className= 'container grid grid-cols-2 text-white'>
+                    <div className= "flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className= 'text-5xl font-bold'>
                             Bem vindo(a)!  
                             </h2>
 
-                            <p
-                                style={{
-                                    fontSize: "3rem",
-                                    fontWeight: "bold",
-                                }}>
+                            <p className= 'text-xl'>
                                 Compartilhe aqui, suas ideias, experiências e conhecimentos.
                             </p>
 
-                            <div 
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "space-around",
-                                    gap: "1rem"
-                                }}>
+                            <div className="flex justify-around gap-4">
 
-                                <div
-                                    style={{
-                                        borderRadius: "0.5rem",
-                                        color: "white",
-                                        border: "2px solid white",
-                                        padding: "0.5rem 1rem",
-                                    }}>
+                                <div className="rounded text-white
+                                    border-white border-solid border-2
+                                    py-2 px-4">
                                     Criar Postagem
                                 </div>
                             </div>
                     </div>
                     
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                        }}>
+                    <div className="flex justify-center">
                          
                         <img
                             src="https://i.imgur.com/fyfri1v.png"
                             alt="Imagem de boas vindas"
-                            style={{
-                                width:"66%"
-                            }}/>
+                            className="w-2/3"
+                            />
                             </div>
                     </div>
                 </div>
